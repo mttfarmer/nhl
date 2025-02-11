a19da1b804f1df1c84bf101c064904b39cbe0e55
matt <mttfarmer@gmail.com>
2025-02-10 18:12:56 -0600

    remove js + flatten app

--- js/src/helpers/nhl_api.js
+++
@@ -1,7 +0,0 @@
-async function getGamePlayByPlay(gameId) {
-    const resp = await fetch(`https://api-web.nhle.com/v1/gamecenter/${gameId}/play-by-play`, { method: 'GET' });
-    const data = await resp.json();
-    return data;
-}
-const t = await getGamePlayByPlay('2023020204');
-export { getGamePlayByPlay };
--- js/src/test.js
+++
@@ -1,5 +0,0 @@
-import * as nhl_api from './helpers/nhl_api.js';
-const a = await nhl_api.getGamePlayByPlay('2023020204');
-for (let play of a.plays) {
-    console.log(play);
-}
